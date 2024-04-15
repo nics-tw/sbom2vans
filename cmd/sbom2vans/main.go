090fa285fb3471b3a2adb435c658664d1bb0a370
Mao-Lin Wang <focaaby@gmail.com>
2024-04-15 17:03:56 +0800

    feat: save cve numbers into the same cve struct

--- cmd/sbom2vans/main.go
+++ cmd/sbom2vans/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strings"
 
 	"github.com/google/osv-scanner/pkg/osvscanner"
@@ -33,24 +34,32 @@ func main() {
 				for _, alias := range vf.Vulnerability.Aliases {
 					if strings.HasPrefix(alias, "CVE") {
 
-						// Check if CVE already exists, if not, add it
 						if !isCVEExist(CVEs, alias) {
 
-							CVEs = append(CVEs, CVE{
-								Name:      vf.Package.Name,
-								Version:   vf.Package.Version,
-								Ecosystem: vf.Package.Ecosystem,
-								CVE:       alias,
-							})
-						}
+							if isPackageExist(CVEs, vf.Package.Name) {
+								// Find the package and add the CVE to the CVE array
+								for i, cve := range CVEs {
+									if cve.Name == vf.Package.Name {
+										CVEs[i].CVE = append(CVEs[i].CVE, alias)
+									}
+								}
+							} else {
 
+								CVEs = append(CVEs, CVE{
+									Name:      vf.Package.Name,
+									Version:   extractVersion(vf.Package.Version),
+									Ecosystem: vf.Package.Ecosystem,
+									CVE:       []string{alias},
+								})
+							}
+						}
 					}
 				}
 			}
 
 			// print all CVEs
 			for _, cve := range CVEs {
-				fmt.Printf("CVE: %s, eco: %s, name: %s, ver: %s!\n", cve.CVE, cve.Ecosystem, cve.Name, cve.Version)
+				fmt.Printf("CVE: %v, eco: %s, name: %s, ver: %s\n", cve.CVE, cve.Ecosystem, cve.Name, cve.Version)
 			}
 
 			if err != nil {
@@ -69,19 +78,45 @@ func main() {
 }
 
 type CVE struct {
-	Name      string `json:"name"`
-	Version   string `json:"version"`
-	Ecosystem string `json:"ecosystem"`
-	CVE       string `json:"cve"`
+	Name      string   `json:"name"`
+	Version   string   `json:"version"`
+	Ecosystem string   `json:"ecosystem"`
+	CVE       []string `json:"cve"`
 }
 
 func isCVEExist(cves []CVE, cve string) bool {
 
 	for _, v := range cves {
-		if v.CVE == cve {
+		for _, c := range v.CVE {
+			if c == cve {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func isPackageExist(cves []CVE, name string) bool {
+
+	for _, v := range cves {
+		if v.Name == name {
 			return true
 		}
 	}
 
 	return false
+}
+
+func ptr[T any](t T) *T {
+	return &t
+}
+
+func extractVersion(input string) string {
+	re := regexp.MustCompile(`[vV]?(\d+\.\d+(\.\d+)?)`)
+	match := re.FindStringSubmatch(input)
+	if len(match) > 1 {
+		return match[1]
+	}
+	return ""
 }
